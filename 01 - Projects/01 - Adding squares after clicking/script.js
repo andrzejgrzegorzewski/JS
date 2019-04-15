ed33399cdefa24fb2b0e6b811f00e7e939b86ad5
Andrzej Grzegorzewski <andrzej.grzegorzewski@gmail.com>
2019-04-15 17:36:19 +0200

    add three argument operator

--- 01 - Projects/01 - Adding squares after clicking/script.js
+++ 01 - Projects/01 - Adding squares after clicking/script.js
@@ -4,23 +4,18 @@ let click = 0;
 
 btn.addEventListener('click', () => {
 
-    // document.querySelector('body').appendChild(document.createElement('div'));
+
+    click++;
+    let divElement = document.createElement('div');
+    divElement.textContent = click;
+
+    // if (click % 5 == 0)
+    //     document.querySelector('body').appendChild(divElement).classList.toggle('circle');
+    // else
+    //     document.querySelector('body').appendChild(divElement);
 
     // or
 
-    click++;
-    let div = document.createElement('div');
-    div.textContent = click;
-    console.log(click);
-
-    if (click % 5 == 0)
-        document.querySelector('body').appendChild(div).classList.toggle('circle');
-    else
-        document.querySelector('body').appendChild(div);
-
-
-    // if (click % 5 == 0) ?
-    //     document.querySelector('body').appendChild(div).classList.toggle('circle') :
-    //     document.querySelector('body').appendChild(div);
+    click % 5 == 0 ? document.querySelector('body').appendChild(divElement).classList.toggle('circle') : document.querySelector('body').appendChild(divElement);
 
 });
