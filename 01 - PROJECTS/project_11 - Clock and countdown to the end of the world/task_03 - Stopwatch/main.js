aae6abf6c7ac02f1b4c0525fe5d1d77459fbd6a0
Andrzej Grzegorzewski <andrzej.grzegorzewski@gmail.com>
2019-05-05 21:45:54 +0200

    finish

--- 01 - PROJECTS/project_11 - Clock and countdown to the end of the world/task_03 - Stopwatch/main.js
+++ 01 - PROJECTS/project_11 - Clock and countdown to the end of the world/task_03 - Stopwatch/main.js
@@ -4,21 +4,33 @@ const div = document.querySelector('div.time div');
 
 let second = 0;
 let flag = true;
+let idInterval;
 
-const count = () => {
-
-    flag ? (btnStart.textContent = "Pause",
-
-        setInterval(() => {
-            second++;
-            div.textContent = second;
-
-            flag != flag;
-        }, 1000)
-
-    ) : (btnStart.textContent = "Start",
-        flag != flag
-    );
+const start = () => {
+    second++;
+    div.textContent = (second / 100).toFixed(2);
 }
 
-btnStart.addEventListener('click', count);
+const timer = () => {
+    if (flag) {
+        flag = !flag;
+        btnStart.textContent = "Pause";
+        idInterval = setInterval(start, 10);
+    } else {
+        flag = !flag;
+        btnStart.textContent = "Start";
+        clearInterval(idInterval);
+    }
+}
+
+
+function reset() {
+    second = 0;
+    div.textContent = "0.00";
+    clearInterval(idInterval);
+    btnStart.textContent = "Start";
+    flag = true;
+}
+
+btnStart.addEventListener('click', timer);
+btnReset.addEventListener('click', reset);
