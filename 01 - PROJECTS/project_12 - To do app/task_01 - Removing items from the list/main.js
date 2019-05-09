2d19b48482a080b14e60c02c2ca6941d6da4c744
Andrzej Grzegorzewski <andrzej.grzegorzewski@gmail.com>
2019-05-09 23:58:57 +0200

    next project

---
+++ 01 - PROJECTS/project_12 - To do app/task_01 - Removing items from the list/main.js
@@ -0,0 +1,21 @@
+const removeTask = () => {
+    // console.log(event.target.parentNode);
+    // event.target.parentNode.remove();
+
+    // or
+
+    // event.target.parentNode.style.textDecoration = "line-through";
+
+    // or
+
+
+    const index = event.target.dataset.key;
+
+
+
+    console.log(document.querySelector(`li[data-key = "${index}"]`));
+
+    document.querySelector(`li[data-key = "${index}"]`).remove();
+}
+
+document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask));
